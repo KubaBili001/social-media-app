3ef3472999467a3294c7f0db9ab6b225fae0d65f
KubaBili001 <kubabili.wawa@gmail.com>
2025-03-15 19:03:28 +0100

    search input on home screen

--- components/sidebar/SideBar.tsx
+++ components/sidebar/SideBar.tsx
@@ -11,32 +11,35 @@ import { MdAddCircleOutline } from "react-icons/md";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import { useState } from "react";
+import { Input } from "../ui/input";
 
 export function AppSidebar() {
   const [isExtended, setIsExtended] = useState<boolean>(false);
 
   const labelStyle = `absolute left-13 opacity-0 ${
     isExtended ? "" : "lg:opacity-100"
-  } transition-all`;
+  } transition-all truncate`;
 
   return (
     <div
-      className={`flex md:flex-col gap-3 items-center transition-[width] md:items-start p-3 border-b md:border-r md:border-b-0 border-border w-full md:w-[var(--sidebar-width-md)] lg:w-[var(--sidebar-width-${
-        isExtended ? "md" : "lg"
-      })] h-[var(--sidebar-height-sm)] md:h-full`}
+      className={`relative flex md:flex-col gap-3 items-center justify-end md:justify-start lg:transition-[width] md:items-start p-3 border-b md:border-r md:border-b-0 border-border w-full md:w-[var(--sidebar-width-md)] ${
+        isExtended
+          ? "lg:w-[var(--sidebar-width-md)]"
+          : "lg:w-[var(--sidebar-width-lg)]"
+      } h-[var(--sidebar-height-sm)] md:h-full`}
     >
       <Link
         href={"/home"}
-        className="flex items-center gap-3 md:w-full p-3 md:my-4 hover:bg-secondary rounded-md transition-colors ease-in relative"
+        className="flex items-center gap-3 md:w-full p-3 md:my-4 hover:bg-secondary rounded-md transition-colors ease-in absolute left-1 md:left-0 md:relative"
       >
-        <RiNextjsFill className="fill-white w-7 h-7" />
+        <RiNextjsFill className="fill-white w-6 h-6" />
         <span className={labelStyle}>Social media app</span>
       </Link>
       <Link
         href={"/home"}
         className="hidden md:flex items-center gap-3 md:w-full p-3 hover:bg-secondary rounded-md transition-colors ease-in relative"
       >
-        <MdHome className="w-7 h-7" />
+        <MdHome className="w-6 h-6" />
         <span className={labelStyle}>Home</span>
       </Link>
       <Button
@@ -45,14 +48,14 @@ export function AppSidebar() {
         size={"clean"}
         onClick={() => setIsExtended((prev) => !prev)}
       >
-        <IoSearch className="w-7 h-7 fill-white" />
+        <IoSearch className="w-6 h-6 fill-white" />
         <span className={labelStyle}>Search</span>
       </Button>
       <Link
         href={"/messages"}
         className="hidden md:flex items-center gap-3 md:w-full p-3 hover:bg-secondary rounded-md transition-colors ease-in relative"
       >
-        <FiSend className="w-7 h-7" />
+        <FiSend className="w-6 h-6" />
         <span className={labelStyle}>Messages</span>
       </Link>
       <Button
@@ -60,7 +63,7 @@ export function AppSidebar() {
         variant={"clean"}
         size={"clean"}
       >
-        <FaRegHeart className="w-7 h-7 fill-white" />
+        <FaRegHeart className="w-6 h-6 fill-white" />
         <span className={labelStyle}>Notifications</span>
       </Button>
       <Button
@@ -68,11 +71,19 @@ export function AppSidebar() {
         variant={"clean"}
         size={"clean"}
       >
-        <MdAddCircleOutline className="w-7 h-7 fill-white" />
+        <MdAddCircleOutline className="w-6 h-6 fill-white" />
         <span className={labelStyle}>Create a post</span>
       </Button>
-      <div className="flex gap-3">
+      <div className="flex gap-3 items-center">
         {/* move input to separate component to handle search logic */}
+        <Input className="md:hidden" placeholder="Search" />
+        <Button
+          className="flex md:hidden items-center gap-3 md:w-full p-3 rounded-md hover:cursor-pointer"
+          variant={"clean"}
+          size={"clean"}
+        >
+          <FaRegHeart className="w-6 h-6 fill-white" />
+        </Button>
       </div>
     </div>
   );
